1505d5c62ee08fc1def80c5cd50bd52a394ce7cc
MateuszKubis137 <122632079+MateuszKubis137@users.noreply.github.com>
2023-03-24 02:22:40 +0100

    Update MySQL_Examples_Database1.sql

--- Other_projects_and_materials/MySQL/MySQL_Examples_Database1.sql
+++ Other_projects_and_materials/MySQL/MySQL_Examples_Database1.sql
@@ -3,13 +3,13 @@
 # Which official languages are the most commonly used in Europe?
 # Soultion using subquery
 SELECT 
-	language, 
-	COUNT(*) liczba
+    language, 
+    COUNT(*) liczba
 FROM countrylanguage
 WHERE 
-	IsOfficial = 'T'
-	AND countrycode IN (
-	SELECT code FROM country WHERE continent = 'Europe')
+    IsOfficial = 'T'
+    AND countrycode IN (
+    SELECT code FROM country WHERE continent = 'Europe')
 GROUP BY language
 ORDER BY liczba DESC;
 
@@ -20,29 +20,29 @@ SELECT
 FROM countrylanguage, country
 WHERE
     IsOfficial = 'T'
-	AND continent = 'Europe'
-	AND countrylanguage.countrycode = country.code
+    AND continent = 'Europe'
+    AND countrylanguage.countrycode = country.code
 GROUP BY language
 ORDER BY liczba DESC;
 
 # Select the names, population, and surface area of all Asian countries, and calculate the population density. The population of each country must be greater than the population of the most populous European country, 
 # and the population density must be greater than the population density of all African countries.
 SELECT
-	name,
+    name,
     population,
     surfaceArea,
     ROUND(population/surfaceArea, 2) `Population density`
 FROM country
 WHERE 
-	continent = 'Asia'
-	AND population > (
-		SELECT MAX(population) 
+    continent = 'Asia'
+    AND population > (
+        SELECT MAX(population) 
         FROM country 
         WHERE 
-			continent = 'Europe' 
+		    continent = 'Europe' 
 			AND name <> 'Russian Federation')
-	AND ROUND(population/surfaceArea, 2) > ALL (
-		SELECT ROUND(population/surfaceArea, 2) 'density' 
+    AND ROUND(population/surfaceArea, 2) > ALL (
+	    SELECT ROUND(population/surfaceArea, 2) 'density' 
         FROM country 
         WHERE continent = 'Africa' 
         order by `density` desc);
@@ -54,10 +54,10 @@ SELECT
     population,
     language
 FROM countrylanguage
-	JOIN country ON countrylanguage.countrycode = country.code
+    JOIN country ON countrylanguage.countrycode = country.code
 WHERE 
-	GNP > 1000 
-	AND countrylanguage.Percentage > 50
+    GNP > 1000 
+    AND countrylanguage.Percentage > 50
     AND isofficial = 'T'
 Order BY continent, country.name; 
 
@@ -78,14 +78,14 @@ GROUP BY country.name;
 SELECT DISTINCT language
 FROM countrylanguage 
 WHERE language IN (
-	SELECT language 
+    SELECT language 
     FROM (
-		SELECT language, COUNT(*) `number of countries`
+	    SELECT language, COUNT(*) `number of countries`
         FROM countrylanguage 
         WHERE IsOfficial = 'F' 
         GROUP BY language
         ) table1 
-	WHERE `number of countries` > 10)
+    WHERE `number of countries` > 10)
 
 EXCEPT
 
@@ -95,21 +95,21 @@ WHERE IsOfficial = 'T';
 
 # Select the maximum average city population for all South American countries, all Oceania countries, and all North American countries.
 SELECT 
-	Continent, 
-	MAX(avg_population) `Maximum average population` 
+    Continent, 
+    MAX(avg_population) `Maximum average population` 
 FROM (
-	SELECT 
-		country.name, 
-		ANY_VALUE(continent) Continent, 
+    SELECT 
+	country.name, 
+        ANY_VALUE(continent) Continent, 
 		AVG(city.population) avg_population
-	FROM city 
-		INNER JOIN country ON city.CountryCode = country.Code
-	WHERE continent IN (
-		'South America', 
+    FROM city 
+        INNER JOIN country ON city.CountryCode = country.Code
+    WHERE continent IN (
+        'South America', 
         'North America', 
         'Oceania')
-	GROUP BY country.name
-	) AS table_1
+    GROUP BY country.name
+    ) AS table_1
 Group by Continent
 ORDER BY MAX(avg_population) 
 ;
