f2af73c779e91e977800c165688407d574a8cb3f
MateuszKubis137 <122632079+MateuszKubis137@users.noreply.github.com>
2023-03-24 02:27:32 +0100

    Update MySQL_Examples_Database2.sql

--- Other_projects_and_materials/MySQL/MySQL_Examples_Database2.sql
+++ Other_projects_and_materials/MySQL/MySQL_Examples_Database2.sql
@@ -2,24 +2,24 @@
 
 # Select all Bands that have Albums and at least 30 songs
 SELECT 
-	ANY_VALUE(bands.name) `Band name`, 
+    ANY_VALUE(bands.name) `Band name`, 
     count(songs.id) `Number of songs` 
 FROM bands
-	INNER JOIN albums ON albums.band_id = bands.id
-	INNER JOIN songs ON albums.id=songs.album_id
+    INNER JOIN albums ON albums.band_id = bands.id
+    INNER JOIN songs ON albums.id=songs.album_id
 GROUP BY bands.id
 HAVING  `Number of songs` >= 30;
 
 # Select all Bands that have No Albums
 SELECT bands.name  
 FROM bands 
-	LEFT JOIN albums on bands.id = albums.band_id 
+    LEFT JOIN albums on bands.id = albums.band_id 
 WHERE albums.id IS NULL;
 
 # Get the Longest Album
 SELECT 
-	a.name, 
-	release_year, 
+    a.name, 
+    release_year, 
     sum(length) AS duration 
 FROM albums a, songs s 
 WHERE a.id=s.album_id 
@@ -52,28 +52,28 @@ ORDER BY id DESC LIMIT 1;
 
 # Select the maximum, minimum, and average length of Songs for each band
 SELECT 
-	bands.name,
-	MAX(length) `Maximum length`,
+    bands.name,
+    MAX(length) `Maximum length`,
     MIN(length) `Minimum length`,
-	ROUND(AVG(length), 2) `Average length`
+    ROUND(AVG(length), 2) `Average length`
 FROM songs
-	INNER JOIN albums ON songs.album_id = albums.id
+    INNER JOIN albums ON songs.album_id = albums.id
     INNER JOIN bands ON bands.id = albums.band_id
 GROUP BY bands.name;
 
 # Retrieve the longest song from each album and sort the results by the length of the longest song on each album
 SELECT 
-	ANY_VALUE(a.name) album, 
-	ANY_VALUE(s.name) song, 
+    ANY_VALUE(a.name) album, 
+    ANY_VALUE(s.name) song, 
     ROUND(max(length), 2) `the longest song`
 FROM songs s 
-	JOIN albums a ON s.album_id = a.id 
+    JOIN albums a ON s.album_id = a.id 
 GROUP BY album_id 
 ORDER BY `the longest song` DESC;
 
 # Select the number of Songs for each Band
 SELECT 
-	b.name, 
+    b.name, 
     count(s.id) number_of_songs 
 FROM bands b, albums a, songs s 
 WHERE b.id = a.band_id AND a.id = s.album_id 
@@ -81,14 +81,14 @@ GROUP  BY b.name;
 
 # Calculate the average length of songs that come from albums with a release year between 1995 and 2010 and start with the letter 'T' for each band.
 SELECT 
-	ANY_VALUE(bands.id) ID, 
+    ANY_VALUE(bands.id) ID, 
     bands.name, 
     AVG(songs.length) 'Average length'  
 FROM bands
-	INNER JOIN albums ON albums.band_id = bands.id
-	INNER JOIN songs on albums.id=songs.album_id
+    INNER JOIN albums ON albums.band_id = bands.id
+    INNER JOIN songs on albums.id=songs.album_id
 where release_year between 1995 AND 2010
-	AND songs.name like "T%"
+    AND songs.name like "T%"
 GROUP BY bands.name
 ORDER BY 'Average length';
 
@@ -106,14 +106,14 @@ WHERE songs.name = albums.name;
 
 # Retrieve all informations about albums and names of the songs released in period other than between 1998-2010. Order by release year, songs name and albums name
 SELECT 
-	albums.*, 
+    albums.*, 
     songs.name 
 FROM songs 
-	INNER JOIN albums ON songs.album_id = albums.id
-	INNER JOIN bands ON albums.band_id = bands.id
+    INNER JOIN albums ON songs.album_id = albums.id
+    INNER JOIN bands ON albums.band_id = bands.id
 WHERE albums.release_year Not between '1998' AND '2010'
 ORDER BY 
-	albums.release_year, 
+    albums.release_year, 
     songs.name, 
     albums.name;
     
